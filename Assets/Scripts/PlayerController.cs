96ad6db4ea5d75f459d0332de76058ae2967554e
Lucas <lcorte110210@gmail.com>
2025-10-05 13:24:30 -0300

    Atualizando controle do jogador

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,30 +6,20 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     float pHorizontal;
     float pVertical;
-    
-    void Update()
+    Rigidbody2D rb;
+
+    void Start()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-        if ((horizontal > 0 && horizontal < pHorizontal) || (horizontal < 0 && horizontal > pHorizontal))
-            horizontal = 0;
-        else
-            pHorizontal = horizontal;
-        if ((vertical > 0 && vertical < pVertical) || (vertical < 0 && vertical > pVertical))
-            vertical = 0;
-        else
-            pVertical = vertical;
-        
-        Debug.Log(horizontal + ", " + vertical);
-
-        if (horizontal != 0)
-            horizontal = Mathf.Sign(horizontal);
-        if (vertical != 0)
-            vertical = Mathf.Sign(vertical);
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
         Vector2 movement = new Vector2(horizontal, vertical);
 
-        transform.Translate(new Vector3(movement.x, movement.y, 0f) * speed * Time.deltaTime, Space.World);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
